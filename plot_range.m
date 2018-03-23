c754785885478330e9c97db3966220a454c14516
Hammad Imam <himam99@iastate.edu>
2018-03-23 02:57:26 -0500

    Finished range files

--- plot_range.m
+++ plot_range.m
@@ -1,4 +1,29 @@
 %Hammad Imam // himam@iastate.edu
 %AERE 161 Project 2
 %Function
-%Plots several flightpaths
+%Plots several range plots
+
+function plot_range(fs)
+
+k = [];
+x = [];
+t = [];
+
+%gather k, tmax, and xmax into vectors
+for i = 1:size(fs,2)
+    k(i) = fs(i).k
+    x(i) = fs(i).x(end)
+    t(i) = fs(i).t(end)
+end
+
+%Range x, Resistance k
+figure
+plot(k, x);
+xlabel('Coefficient of Resistance, k, [1/s]');
+ylabel('Range, x_max, [m]');
+
+%Total Time t, Resistance k
+figure
+plot(k, t);
+xlabel('Coefficient of Resistance, k, [1/s]');
+ylabel('Total Time, t_max, [t]');
